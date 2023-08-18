1fd71518ae34457a37d8feb1c58c865bc7ba4ecf
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2023-08-18 22:37:39 +0530

    Update power_of_two.py

--- power_of_two.py
+++ power_of_two.py
@@ -1,3 +1,5 @@
+#qus:-#qus:- Given an integer n, return true if it is a power of two. Otherwise, return false. An integer n is a power of two, if there exists an integer x such that n == 2x.
+
 def power(n):
     if n<=0:
         return False
