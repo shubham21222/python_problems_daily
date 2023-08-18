8a5b1391bd1aa45939d7dbae57f485d336930ad0
shubham21222 <shubhamraikwar@hotmail.com>
2023-08-18 21:55:50 +0530

    reverse linked list

---
+++ reverse_list.py
@@ -0,0 +1,14 @@
+#qus:- Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        return prev
