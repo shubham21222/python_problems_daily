0b84f6839bbc0772f91a6dc739c61b36bd8c1540
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2024-04-20 11:11:39 +0530

    Time: 29 ms (96.32%) | Memory: 16.5 MB (41.96%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        r = x
+        if x == 0:
+            return 0
+
+        for i in range(20):
+            r = (r + x/r) /2 
+
+        return math.floor(r)
